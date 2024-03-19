85e7b35994efc5490f867123215fbc81dafb8256
Ngo Minh Tri <ngominhtrint@gmail.com>
2024-03-19 12:26:06 -0500

    Implemented privacy policy page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./App.scss";
 import Main from "./containers/Main";
+import Privacy from "./containers/privacy/Privacy";
+import { useLocation, Route, Routes } from "react-router-dom";
 
 function App() {
+  const location = useLocation();
+  const state = location.state;
+
   return (
-    <div>
-      <Main />
-    </div>
+    
+      <Routes location={state?.backgroundLocation || location}>
+        <Route path="/" element={<Main />} />
+        <Route path="privacy" element={<Privacy />} />
+      </Routes>
+    
   );
 }
 
---
+++ src/containers/privacy/Privacy.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./Privacy.scss";
+
+const Privacy = () => {
+
+    return (
+      <div className="main">
+        <h1 className="page-title">Privacy Policy</h1>
+        <div className="body">
+            <p>Effective date: <b>March 19th, 2024</b></p>
+            <p>This Privacy Policy describes how our app ("us", "we", or "our") collects, uses, and shares information when you use our mobile application (the "App").
+</p>
+            <h4>Information Collection and Use</h4>
+            <p>We do not collect any personal information from users of our app. We do not require users to create accounts or provide any personal data such as names, email addresses, or phone numbers.</p>
+
+            <h4>Log Data</h4>
+            <p>Our app does not collect log data from users. We do not track user activities within the app or collect any information about how the app is used.</p>
+            
+            <h4>Cookies</h4>
+            <p>Our app does not use cookies. We do not store any information on users' devices through cookies or similar tracking technologies.</p>
+            <p>Please note that since we do not collect any personal information or track user activities, we do not use the collected data for any purposes, including advertising, analytics, or improving user experience. Our priority is to provide a safe and enjoyable experience for our users without compromising their privacy.</p>
+            
+            <h4>Changes to This Privacy Policy</h4>
+            <p>We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page.</p>
+            
+            <h4>Contact Us</h4>
+            <p>If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us <a href="https://ngominhtrint.github.io/">HERE</a></p>
+            <p>This Privacy Policy page was created with the help of the Privacy Policy Generator and adapted for use in our products.</p>
+        </div>
+      </div>
+    );
+  };
+  
+  export default Privacy;
